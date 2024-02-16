a2f1ef5c767261a25dd292fb4cefe9cadc286985
unknown <you@example.com>
2024-02-16 01:30:01 -0300

    deploy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+import "dotenv/config";
+
 import app from "./app/app.js";
 
 const port = process.env.PORT || 3001;
+
+console.log(process.env);
 
 app.listen(port, () => {
  console.log(
--- src/models/connection.js
+++ src/models/connection.js
@@ -1,7 +1,18 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize("eng", "root", "123456", {
+/* export const sequelize = new Sequelize("eng", "root", "123456", {
  host: "localhost",
  dialect: "mysql",
  port: 3306,
-});
+}); */
+
+export const sequelize = new Sequelize(
+ "sql10684457",
+ "sql10684457",
+ "EQBdp9HW5g",
+ {
+  host: "sql10.freesqldatabase.com",
+  dialect: "mysql",
+  port: 3306,
+ }
+);
